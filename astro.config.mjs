3fcefdeb4c0cf221ab18f6013f6a5c52c9dfc7f6
hanamanajun-sudo <hanamanajun@gmail.com>
2026-03-27 13:34:53 +0900

    fix: add React integration for Keystatic, add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
 import keystatic from '@keystatic/astro';
+import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://trip.lalalakorea.com',
   output: 'server',
   adapter: vercel({ webAnalytics: { enabled: false } }),
-  integrations: [tailwind(), keystatic()],
+  integrations: [tailwind(), react(), keystatic()],
   markdown: {
     shikiConfig: {
       theme: 'github-light',
